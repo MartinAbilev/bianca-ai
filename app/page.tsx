f77565433ab82b61c68f305f5954417dfc2ba21d
martin <martins.abilevs@sapiens.com>
2024-09-04 08:15:38 +0300

    main  background json wrap

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default  function Page()
     <div id="app">
 
       {/* ***FOREGROUND*** */}
-      <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <div className="flex min-h-screen flex-col items-center justify-between p-8">
 
         <div className="flex flex-col items-center">
           {/* Bugz Powered and about us */}
@@ -130,82 +130,54 @@ export default  function Page()
         }
         </div>
 
-        {/* usseles button */}
-        <div className="font-mono p-4">
-          {/* <button className="button bc-5 br-4 font-mono" onClick={()=>setValue(value + 1)}> update </button> */}
-        </div>
-      </main>
+
+      </div>
+
+
 
       {/* ***BACKGROUND*** */}
-      <main className="before flex min-h-screen flex-col items-center justify-between p-24">
-        {/* cool background fx */}
-        <div className="font-mono p-6">
-          <div className="relative flex place-items-center
-          before:absolute
-          before:h-[1250px]
-          before:w-[1080px]
-          after:h-[580px]
-          after:w-[540px]
-          before:lg:h-[660px]
-          before:-translate-x-1/2
-          before:rounded-full
-          before:bg-gradient-radial
-          before:from-white
-          before:to-transparent
-          before:blur-2xl
-          before:content-['']
-          after:absolute
-          after:-z-20
-          after:translate-x-1/3
-          after:bg-gradient-conic
-          after:from-sky-200
-          after:via-blue-200
-          after:blur-2xl
-          after:content-['']
-          before:dark:bg-gradient-to-br
-          before:dark:from-transparent
-          before:dark:to-blue-700
-          before:dark:opacity-10
-          after:dark:from-sky-900
-          after:dark:via-[#0141ff]
-          after:dark:opacity-40
-          z-[-1]">
-          </div>
-        </div>
+      <div className="before"  style={{width: "width:100%"}}>
 
         {/* just divider */}
         <div className="font-mono p-4"></div>
 
         {/* cool matrix fx in background */}
-        <div className="font-mono p-4 before opacity-10">
+        <div className="font-mono p-4 before opacity-10"  style={{width: "width:100%", wordBreak: "break-all"}}>
           state: {JSON.stringify(data)}
         </div>
-      </main>
 
-      {/* cool bug fx */}
-      {
-        data && data.bugz.map(((a:any, i: number)=>
+        {/* cool bug fx */}
         {
-          return <div className="p-8" key={i}>
-            <div
-            className="before"
-            style={{
-            color: "white",
-            position: 'absolute',
-            top: `${data.bugz[i].y}px`,
-            left: `${data.bugz[i].x}px`,
-            height: '24px',
-            zIndex: -10,
-            padding: 0,
-            backgroundBlendMode: 'owerlay',
-            backgroundColor: 'lightblue',
-            transition:'left 1s linear',
-            opacity: '30%'
-            }}
-            >|O|</div>
-          </div>
-        }))
-      }
+          <div style={{display: "block", width: "width:100%", wordBreak: "break-all"}}>
+
+            {data && data.bugz.map(((a:any, i: number)=>
+            {
+              return <div className="p-8" key={i}>
+                <div
+                className="before"
+                style={{
+                display: "block",
+                color: "white",
+                position: 'absolute',
+                top: `${data.bugz[i].y/6}%`,
+                left: `${data.bugz[i].x/8}%`,
+                right: '10px',
+                height: '24px',
+                zIndex: -10,
+                padding: 0,
+                backgroundBlendMode: 'owerlay',
+                backgroundColor: 'lightblue',
+                transition:'left 1s linear',
+                opacity: '30%'
+                }}
+                >|O|</div>
+              </div>
+            }))
+          }
+          </div>}
+        </div>
+
+
 
     </div>
   )
