d683ec59fb31dc3263c6a7d51dc3a34ad36d57d9
martin <martins.abilevs@sapiens.com>
2024-09-12 09:39:38 +0300

    up

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default  function Page()
     <div id="app">
 
       {/* ***FOREGROUND*** */}
-      <div className="flex min-h-screen flex-col items-center justify-between p-8">
+      <div className="flex min-h-screen flex-col items-center p-8">
 
         <div className="flex flex-col items-center">
           {/* Bugz Powered and about us */}
