c2a34ea0d5703630dbde161c9d634031dff3bda1
Martin <martin.abilev@gmail.com>
2024-10-30 13:23:42 +0200

    update style

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function BiDashPage()
 {
     return (
-    <div className="container flex min-h-screen items-center justify-center font-extrabold text-xl text-gray-500">
+    <div className="flex min-h-screen items-center justify-center font-extrabold text-xl text-gray-500">
         BI DASHBOARD PAGE
     </div>)
 }
