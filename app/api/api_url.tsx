aedb8b20d1d4adc8f248633d85c46099c4b817cd
Martin <martin.abilev@gmail.com>
2024-11-03 20:24:24 +0200

    set directly to bzig server fixed port

--- app/api/api_url.tsx
+++ app/api/api_url.tsx
@@ -1,6 +1,7 @@
 const API_URL:any =
 {
-  production: 'http://77.38.218.82:3000/api/state', // uncoment this for prod
+  production: 'http://77.38.218.82:3001/api', // uncoment this for prod
+  // production: 'http://77.38.218.82:3000/api/state', // uncoment this for prod
   // production: 'http://host.docker.internal:3001/api/', // uncoment this for local build
   // production: 'http://localhost:3001/api', // uncoment this for local build
   development:'http://localhost:3001/api'// uncoment this for local bug server
