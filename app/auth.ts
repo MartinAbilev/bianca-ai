82427cd718afbed8d860779003e7d029bcffeb49
Martin <martin.abilev@gmail.com>
2025-01-06 15:17:08 +0200

    feat : dashboard get session

--- app/auth.ts
+++ app/auth.ts
@@ -11,7 +11,7 @@ import { authConfig } from './auth.config';
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    return user.rows[0] || null;
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,30 @@
+"use client"
+import { useEffect, useState } from 'react'
+import { getSession } from 'next-auth/react'
+import { Session } from 'next-auth'
+
 export default function BiDashPage()
 {
+    const [session, setSession] =  useState<Session | null>(null)
+
+    useEffect(() =>
+    {
+        const storeUserId = async () =>
+        {
+            console.log('Get Session..')
+            const session = await getSession()
+            console.log('sesion:', session)
+            if (session)
+            {
+                localStorage.setItem('session', JSON.stringify(session))
+                setSession(session)
+            }
+        }
+        storeUserId()
+     }, [])
+
     return (
     <div className="flex min-h-screen items-center justify-center font-extrabold text-xl text-gray-500">
-        BI DASHBOARD PAGE
+        {session && session.user && session.user.name} BI DASHBOARD PAGE.
     </div>)
 }
